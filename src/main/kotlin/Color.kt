0497ac39ffae6d0bbb2663f1376fd000f0a593d7
Ludovico Morabito <ludovico.morabito@gmail.com>
2024-03-06 12:36:25 +0100

    Color.kt file aded to the repository

--- src/main/kotlin/Color.kt
+++ src/main/kotlin/Color.kt
@@ -1,6 +1,11 @@
 package org.example
 
-data class Color()
-{
+data class Color(var r:Float=0f, var g:Float=0f, var b:Float=0f )
+/**
+ * the Color class take as argument 3 float variables:
+ * r (red component of a pixel),
+ * b (blue component of a pixel),
+ * g (green component of a pixel),
+ * the default values are (0.,0.,0.)
+ */
 
-}
