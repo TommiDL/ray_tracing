f5a9f899aa94bb0c95cec24d27a447dafd1bfe46
Ludovico Morabito <ludovico.morabito@gmail.com>
2024-03-27 13:17:45 +0100

    implementation of ToString function and are_close function of the Point class

--- src/main/kotlin/Point.kt
+++ src/main/kotlin/Point.kt
@@ -1,3 +1,21 @@
 package org.example
 
-data class Point()
+data class Point(var x:Float, var y:Float, var z:Float){
+    /**
+     * this function converts to a string the values of the Point class
+     */
+    override fun toString():String{
+        return "Point (x$x, y$y, z$z)"
+    }
+    /**
+     * check if two Points are similar within a certain precision
+     */
+    fun are_close(a:Point):Boolean{
+        return (are_similar(this.x, a.x, eps = 1e-5f) and are_similar(this.y, a.y, eps = 1e-5f) and are_similar(this.z, a.z, eps = 1e-5f))
+    }
+
+
+
+
+
+}
