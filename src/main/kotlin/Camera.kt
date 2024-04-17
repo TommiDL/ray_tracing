6dce8ea542a8d77c66642d112300be7127762a4f
Maria Laura <marialaura.ilisco@studenti.unimi.it>
2024-04-17 12:46:56 +0200

    implementation of class Cameras (fun fire_ray)

--- src/main/kotlin/Camera.kt
+++ src/main/kotlin/Camera.kt
@@ -1,10 +1,32 @@
 package org.example
 
+import org.gradle.internal.impldep.org.glassfish.jaxb.core.v2.util.EditDistance
+
 interface Camera {
+    fun fire_ray (u:Float, v:Float):Ray
 }
 
-class OrthogonalCamera()
-{}
+class OrthogonalCamera(val aspect_ratio:Float=1.0f,
+                       val transformation: Transformation = Transformation(),
+    ) : Camera
+{
+    override fun fire_ray (u:Float, v:Float):Ray{
+        val origin:Point = Point(-1.0f, (1.0f - 2 * u) * this.aspect_ratio, 2 * (v - 1))
+        val direction:Vec = Vec(1, 0, 0)
+        return Ray(origin = origin, dir = direction, tmin = 1.0e-5f).transform(this.transformation)
+    }
 
-class PerspectiveCamera()
-{}
+}
+
+class PerspectiveCamera(val distance:Float=1.0f,
+                        val aspect_ratio:Float=1.0f,
+                        val transformation: Transformation = Transformation(),
+    ) : Camera
+{
+    override fun fire_ray (u:Float, v:Float):Ray{
+        val origin:Point = Point(-this.distance, 0.0f, 0.0f)
+        val direction:Vec = Vec(this.distance, (1.0f - 2 * u) * this.aspect_ratio, 2 * (v - 1))
+        return Ray(origin = origin, dir = direction, tmin = 1.0e-5f).transform(this.transformation)
+    }
+
+}
