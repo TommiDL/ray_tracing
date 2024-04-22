ed9af5948defe6134e20ef8483de414850076b94
Maria Laura <marialaura.ilisco@studenti.unimi.it>
2024-04-22 12:11:23 +0200

    debugging in class CameraTest.kt

--- src/test/kotlin/CameraTest.kt
+++ src/test/kotlin/CameraTest.kt
@@ -13,66 +13,66 @@ class CameraTest {
     @Test
     fun testOrthogonalCamera():CameraTest {
 
-        var cam = OrthogonalCamera(aspect_ratio=2.0)
+        var cam = OrthogonalCamera(aspect_ratio=2.0f)
 
-        var ray1 = cam.fire_ray(0.0, 0.0)
-        var ray2 = cam.fire_ray(1.0, 0.0)
-        var ray3 = cam.fire_ray(0.0, 1.0)
-        var ray4 = cam.fire_ray(1.0, 1.0)
+        var ray1 = cam.fire_ray(0.0f, 0.0f)
+        var ray2 = cam.fire_ray(1.0f, 0.0f)
+        var ray3 = cam.fire_ray(0.0f, 1.0f)
+        var ray4 = cam.fire_ray(1.0f, 1.0f)
 
         //Verify that the rays are parallel by verifying that cross-products vanish
-        assertTrue are_close(0.0, ray1.dir.prod(ray2.dir).squared_norm())
-        assertTrue are_close(0.0, ray1.dir.prod(ray3.dir).squared_norm())
-        assertTrue are_close(0.0, ray1.dir.prod(ray4.dir).squared_norm())
+        assertTrue(are_close(0.0f, ray1.dir.prod(ray2.dir).squared_norm()))
+        assertTrue(are_close(0.0f, ray1.dir.prod(ray3.dir).squared_norm()))
+        assertTrue(are_close(0.0f, ray1.dir.prod(ray4.dir).squared_norm()))
 
         //Verify that the ray hitting the corners have the right coordinates
-        assertTrue ray1.at(1.0).is_close(Point(0.0, 2.0, -1.0))
-        assertTrue ray2.at(1.0).is_close(Point(0.0, -2.0, -1.0))
-        assertTrue ray3.at(1.0).is_close(Point(0.0, 2.0, 1.0))
-        assertTrue ray4.at(1.0).is_close(Point(0.0, -2.0, 1.0))
+        assertTrue(ray1.at(1.0f).is_close(Point(0.0f, 2.0f, -1.0f)))
+        assertTrue(ray2.at(1.0f).is_close(Point(0.0f, -2.0f, -1.0f)))
+        assertTrue(ray3.at(1.0f).is_close(Point(0.0f, 2.0f, 1.0f)))
+        assertTrue(ray4.at(1.0f).is_close(Point(0.0f, -2.0f, 1.0f)))
 
     }
 
     @Test
     fun testOrthogonalCamera_transform():CameraTest {
 
-        var cam = OrthogonalCamera(transformation=translation(Vec(0,-1,0) * 2.0) * rotation((0,0,1), 90))
+        var cam = OrthogonalCamera(transformation=translation(Vec(0f,-1f,0f) * 2.0f) * rotation(Vec(0f,0f,1f), 90))
 
-        var ray = cam.fire_ray(0.5, 0.5)
-        asserTrue ray.at(1.0).is_close(Point(0.0, -2.0, 0.0))
+        var ray = cam.fire_ray(0.5f, 0.5f)
+        asserTrue(ray.at(1.0f).is_close(Point(0.0f, -2.0f, 0.0f)))
 
     }
 
     @Test
     fun testPerspectiveCamera():CameraTest {
 
-    var cam = PerspectiveCamera(screen_distance=1.0, aspect_ratio=2.0)
+    var cam = PerspectiveCamera(screen_distance=1.0f, aspect_ratio=2.0f)
 
-    var ray1 = cam.fire_ray(0.0, 0.0)
-    var ray2 = cam.fire_ray(1.0, 0.0)
-    var ray3 = cam.fire_ray(0.0, 1.0)
-    var ray4 = cam.fire_ray(1.0, 1.0)
+    var ray1 = cam.fire_ray(0.0f, 0.0f)
+    var ray2 = cam.fire_ray(1.0f, 0.0f)
+    var ray3 = cam.fire_ray(0.0f, 1.0f)
+    var ray4 = cam.fire_ray(1.0f, 1.0f)
 
     //Verify that all the rays depart from the same point
-    assertTrue ray1.origin.is_close(ray2.origin)
-    assertTrue ray1.origin.is_close(ray3.origin)
-    assertTrue ray1.origin.is_close(ray4.origin)
+    assertTrue(ray1.origin.is_close(ray2.origin))
+    assertTrue(ray1.origin.is_close(ray3.origin))
+    assertTrue(ray1.origin.is_close(ray4.origin))
 
     //Verify that the ray hitting the corners have the right coordinates
-    assertTrue ray1.at(1.0).is_close(Point(0.0, 2.0, -1.0))
-    assertTrue ray2.at(1.0).is_close(Point(0.0, -2.0, -1.0))
-    assertTrue ray3.at(1.0).is_close(Point(0.0, 2.0, 1.0))
-    assertTrue ray4.at(1.0).is_close(Point(0.0, -2.0, 1.0)
+    assertTrue(ray1.at(1.0f).is_close(Point(0.0f, 2.0f, -1.0f)))
+    assertTrue(ray2.at(1.0f).is_close(Point(0.0f, -2.0f, -1.0f)))
+    assertTrue(ray3.at(1.0f).is_close(Point(0.0f, 2.0f, 1.0f)))
+    assertTrue(ray4.at(1.0f).is_close(Point(0.0f, -2.0f, 1.0f))
 
     }
 
     @Test
     fun testPerspectiveCamera_transform():CameraTest {
 
-        var cam = PerspectiveCamera(transformation=translation(Vec(0,-1,0) * 2.0) * rotation((0,0,1), 90))
+        var cam = PerspectiveCamera(transformation=translation(Vec(0f,-1f,0f) * 2.0f) * rotation(Vec(0f,0f,1f), 90))
 
-        var ray = cam.fire_ray(0.5, 0.5)
-        asserTrue ray.at(1.0).is_close(Point(0.0, -2.0, 0.0))
+        var ray = cam.fire_ray(0.5f, 0.5f)
+        asserTrue(ray.at(1.0f).is_close(Point(0.0f, -2.0f, 0.0f)))
 
     }
 
