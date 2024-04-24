9865f55e360a03b1bb38fd32220037d423974d9c
Ludovico Morabito <ludovico.morabito@gmail.com>
2024-04-24 16:01:10 +0200

    little changes in Sphere.kt

--- src/main/kotlin/Sphere.kt
+++ src/main/kotlin/Sphere.kt
@@ -1,8 +1,11 @@
 package org.example
 
-class Sphere():Shape {
+class Sphere(val trasformation:Transformation=Transformation()):Shape {
 
     override fun ray_intersection(ray: Ray): HitRecord? {
+
+        val inv_ray:Ray = ray.transform(this.trasformation.inverse())
+        val ray1 =
         if (false) return null
         else return HitRecord()
     }
