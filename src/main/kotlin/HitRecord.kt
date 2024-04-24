54f069c6fdfc861231c594281064544091c72b59
Maria Laura <marialaura.ilisco@studenti.unimi.it>
2024-04-24 17:47:08 +0200

    debugging2 in HitRecord and Vec2D classes

--- src/main/kotlin/HitRecord.kt
+++ src/main/kotlin/HitRecord.kt
@@ -21,7 +21,7 @@ class HitRecord(val world_point:Point, val normal:Normal, val surface_point:Vec2
     fun is_close(hit_record:HitRecord, eps:Float=1e-5f):Boolean
     {
         return (this.world_point.is_close(hit_record.world_point, eps=eps) and
-                this.normal.is_close(hit_record.dir, eps = eps) and
+                this.normal.is_close(hit_record.normal, eps = eps) and
                 this.surface_point.is_close(hit_record.surface_point, eps = eps)
                 )
     }
