e97717a0aca4172e17b9b4eaa49b375b844a4578
Maria Laura <marialaura.ilisco@studenti.unimi.it>
2024-04-24 17:28:36 +0200

    implementation of HitRecord and Vec2D classes

--- src/main/kotlin/HitRecord.kt
+++ src/main/kotlin/HitRecord.kt
@@ -1,4 +1,29 @@
 package org.example
 
-class HitRecord {
+class HitRecord(val world_point:Point, val normal:Normal, val surface_point:Vec2D, val t:Float, val ray:Ray) {
+
+    fun createHitRecord(var inv_ray:Ray,
+                        var first_hit_t:Float,
+                        var transformation:Transformation,
+                        var ray:Ray):HitRecord
+    {
+
+        val hit_point:Point = inv_ray.at(first_hit_t)
+
+        return HitRecord(world_point = this.transformation * hit_point,
+            normal = this.transformation * _sphere_normal(hit_point, ray.dir),
+            surface_point = _sphere_point_to_uv(hit_point),
+            t = first_hit_t ,
+            ray = ray,
+            )
+    }
+
+    fun is_close(hit_record:HitRecord, eps:Float=1e-5f):Boolean
+    {
+        return (this.world_point.is_close(hit_record.world_point, eps=eps) and
+                this.normal.is_close(hit_record.dir, eps = eps) and
+                this.surface_point.is_close(hit_record.surface_point, eps = eps)
+                )
+    }
+
 }
--- src/main/kotlin/Vec2D.kt
+++ src/main/kotlin/Vec2D.kt
@@ -1,4 +1,8 @@
 package org.example
 
-class Vec2D {
+data class Vec2D(var u:Float=0f, var v:Float=0f) {
+    fun is_close(vec2d:Vec2D, eps:Float=1e-5f):Boolean
+    {
+        return (this.u.is_close(vec2d.u, eps=eps) and this.v.is_close(vec2d.v, eps = eps))
+    }
 }
