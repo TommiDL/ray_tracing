fd8b67839919e95744a313084670a2e853486d09
Ludovico Morabito <ludovico.morabito@gmail.com>
2024-04-24 12:07:29 +0200

    bug fixed in ImageTracerTest.kt class importation

--- src/test/kotlin/ImageTracerTest.kt
+++ src/test/kotlin/ImageTracerTest.kt
@@ -1,4 +1,4 @@
-import org.example.Color
+import org.example.*
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
@@ -12,14 +12,14 @@ class ImageTracerTest
         val imageTracer = ImageTracer(image, camera)
 
 
-        val ray1 = imageTracer.fire_ray(0,0, u_pixel = 2.5, v_pixel = 1.5)
-        val ray2 = imageTracer.fire_ray(2, 1, u_pixel = 0.5, v_pixel = 0.5)
+        val ray1 = imageTracer.fire_ray(0,0, u_pixel = 2.5f, v_pixel = 1.5f)
+        val ray2 = imageTracer.fire_ray(2, 1, u_pixel = 0.5f, v_pixel = 0.5f)
         assert(ray1.is_close(ray2))
 
-        imageTracer.fire_all_ray( {ray:Ray -> Color(1f,2f,3f) } )
+        imageTracer.fire_all_ray( {ray: Ray -> Color(1f,2f,3f) } )
         for (row in 0 until image.height) {
             for (col in 0 until image.width) {
-                assert(image.getPixel(col, row) == Color(1f, 2f, 3f))
+                assert(image.get_pixel(col, row) == Color(1f, 2f, 3f))
             }
         }
     }
