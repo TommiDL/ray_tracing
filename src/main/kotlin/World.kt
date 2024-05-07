8b048d58538ef73d672758579424c8ee73f2938f
Ludovico Morabito <ludovico.morabito@gmail.com>
2024-05-07 19:06:54 +0200

    added file World.kt

--- src/main/kotlin/World.kt
+++ src/main/kotlin/World.kt
@@ -1,4 +1,23 @@
 package org.example
 
 class World {
+    var list_shapes: MutableList<Shape> = mutableListOf()
+    fun add(shape: Shape) {
+        list_shapes.add(shape)
+    }
+
+    fun rayIntersection(ray: Ray): HitRecord? {
+        var closest: HitRecord? = null
+        for (shape in list_shapes) {
+            val intersection = shape.ray_intersection(ray)
+
+            if (intersection == null) {
+                continue
+            }
+
+            closest = intersection
+        }
+        return closest
+    }
 }
+
