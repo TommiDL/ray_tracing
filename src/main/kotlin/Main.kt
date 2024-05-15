dbc347943470057c2e73331abfb25e22623f9478
Tommaso Di Luciano <tommaso.diluciano@studenti.unimi.it>
2024-05-15 15:52:00 +0200

    Bugged version of command line interface implementation

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,8 @@
 package org.example
 
+import com.github.ajalt.clikt.core.subcommands
+
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main(argv:Array<String>) {
-
-//    val selection:Selection.main(argv)
-
-
-
-
-
-}
+fun main(argv:Array<String>) = Selection().subcommands(Demo(), pfm2png()).main(argv)
