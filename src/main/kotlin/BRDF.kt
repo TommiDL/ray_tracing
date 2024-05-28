6c0ada5cce1c1dba152237f93f4318740cde259f
Tommaso Di Luciano <tommaso.diluciano@studenti.unimi.it>
2024-05-28 18:38:31 +0200

    Implementation of scatter_ray in DiffusiveBRDF

--- src/main/kotlin/BRDF.kt
+++ src/main/kotlin/BRDF.kt
@@ -1,6 +1,9 @@
 package org.example
 
 import kotlin.math.PI
+import kotlin.math.cos
+import kotlin.math.sin
+import kotlin.math.sqrt
 
 /**
  * BRDF:
@@ -19,7 +22,18 @@ open class BRDF(val pigment: Pigment = UniformPigment(Color(255f, 255f, 255f)))
     {
         return Color(0f,0f,0f)
     }
-    
+
+    open fun scatter_ray(
+        pcg:PCG,
+        incoming_dir:Vec,
+        interaction_point:Point,
+        normal: Normal,
+        depth:Int
+    ):Ray
+    {
+        return Ray()
+    }
+
 }
 
 /**
@@ -30,5 +44,29 @@ class DiffusiveBRDF(pigment: Pigment, val reflectance:Float=1f):BRDF(pigment)
     override fun eval(normal: Normal, in_dir: Vec, out_dir: Vec, uv: Vec2D):Color
     {
         return  this.pigment.get_color(uv) * (this.reflectance/ PI.toFloat())
+    }
+
+    override fun scatter_ray(
+        pcg: PCG,
+        incoming_dir: Vec,
+        interaction_point: Point,
+        normal: Normal,
+        depth: Int
+    ):Ray {
+        val e:Array<Vec> = create_onb_from_z(normal)
+
+        val cos_theta_sq:Float=pcg.random().toFloat()
+
+        val cos_theta:Float= sqrt(cos_theta_sq)
+        val sin_theta:Float= sqrt(1-cos_theta_sq)
+
+        val phi:Float= 2f*PI.toFloat()*pcg.random().toFloat()
+
+        return Ray(
+            dir=e[1]* cos(phi)*cos_theta+e[2]*sin(phi)*cos_theta +e[3]*sin_theta,
+            tmin = 1e-3f,
+            tmax = Float.POSITIVE_INFINITY,
+            depth = depth
+        )
     }
 }
