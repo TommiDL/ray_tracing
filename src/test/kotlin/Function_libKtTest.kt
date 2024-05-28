331cb8c0a309f8e4ce8bb5efce99190500d83b60
Tommaso Di Luciano <tommaso.diluciano@studenti.unimi.it>
2024-05-28 18:16:58 +0200

    Test Implementation for ONB creation

--- src/test/kotlin/Function_libKtTest.kt
+++ src/test/kotlin/Function_libKtTest.kt
@@ -1,7 +1,11 @@
 
 
+import org.example.PCG
+import org.example.Vec
 import org.example.are_similar
+import org.example.create_onb_from_z
 import org.junit.jupiter.api.Test
+import kotlin.test.assertTrue
 
 class Function_libKtTest {
 
@@ -11,4 +15,39 @@ class Function_libKtTest {
     }
 
 
+    @Test
+    fun test_onb_from_z()
+    {
+        val pcg: PCG = PCG()
+
+        for(i in 0 until  1000000)
+        {
+            val normal=Vec(
+                x=pcg.random().toFloat(),
+                y=pcg.random().toFloat(),
+                z=pcg.random().toFloat()
+            ).normalize()
+
+            val e1:Vec= create_onb_from_z(normal)[0]
+            val e2:Vec= create_onb_from_z(normal)[1]
+            val e3:Vec= create_onb_from_z(normal)[2]
+
+            assertTrue(e3.is_close(normal))
+
+            assertTrue(are_similar(0f, e1*e2))
+            assertTrue(are_similar(0f, e2*e3))
+            assertTrue(are_similar(0f, e3*e1))
+
+            assertTrue(are_similar(1f, e1.squared_norm()))
+            assertTrue(are_similar(1f, e2.squared_norm()))
+            assertTrue(are_similar(1f, e3.squared_norm()))
+
+            assertTrue(e1.prod(e2).is_close(e3, eps = 1e-5f) )
+            assertTrue(e2.prod(e3).is_close(e1, eps = 1e-5f) )
+            assertTrue(e3.prod(e1).is_close(e2, eps = 1e-5f) )
+        }
+
+    }
+
 }
+
