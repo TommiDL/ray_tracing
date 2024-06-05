a9c145719019f44d89b2e3a0371a6126468347f9
Tommaso Di Luciano <tommaso.diluciano@studenti.unimi.it>
2024-06-05 12:21:18 +0200

    Error fixed: old implementation called in main

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,4 +6,4 @@ import pfm2png
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main(argv:Array<String>) = Selection().subcommands(Demo(), pfm2png(), pathtracing()).main(argv)
+fun main(argv:Array<String>) = Selection().subcommands(Demo(), pfm2png()).main(argv)
