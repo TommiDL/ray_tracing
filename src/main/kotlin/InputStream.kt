5be64db094b936066927e481ce805fabd496afcb
Maria Laura <marialaura.ilisco@studenti.unimi.it>
2024-06-18 17:08:31 +0200

    updated version of InputStream & InputStreamTest (lesson 13)

--- src/main/kotlin/InputStream.kt
+++ src/main/kotlin/InputStream.kt
@@ -24,15 +24,16 @@ class InputStream {
     }
 
     var location: SourceLocation
-    var saved_char: Char?
+    var saved_char: Char
     var saved_location: SourceLocation
+    var saved_token:Token? = null
 
     /**
      *         """Read a new character from the stream with a check on the end of the stream (byte == -1)"""
      */
-    fun read_char(): Char? {
+    fun read_char(): Char {
 
-        val ch: Char?
+        val ch: Char
 
         if (saved_char != '\u0000') {
             ch = saved_char
@@ -51,7 +52,7 @@ class InputStream {
     /**
      *         """Push a character back to the stream"""
      */
-    fun unread_char(ch: Char?) {
+    fun unread_char(ch: Char) {
         require(this.saved_char == '\u0000')
         this.saved_char = ch
         this.location = this.saved_location.copy()
@@ -99,6 +100,121 @@ class InputStream {
                 this.location.col_num += 1
             }
         }
+    }
+
+    /**
+     *         """Read a token from the stream"""
+     */
+    fun read_token():Token? {
+        val SYMBOLS = listOf('(', ',', ')', '{', '}', '[', ']', ';', ':', '!', '?', '*', '/')
+        var result:Token?
+
+        if(this.saved_token != null){
+            result = this.saved_token
+            this.saved_token = null
+            return result
+        }
+
+        this.skip_whitespaces_and_comments()
+
+        //At this point we're sure that ch does *not* contain a whitespace character
+        val ch:Char = this.read_char()
+        if(ch == '\u0000'){
+            return Token.StopToken(location=this.location)
+        }
+
+        //At this point we must check what kind of token begins with the "ch" character (which has been
+        //put back in the stream with self.unread_char). First, we save the position in the stream
+        val token_location:SourceLocation = this.location.copy()
+
+        if(ch in SYMBOLS) {
+            //One-character symbol, like '(' or ','
+            return Token.SymbolToken(token_location, ch.toString())
+        } else if(ch =='"'){
+            //A literal string (used for file names)
+            return this._parse_string_token(token_location=token_location)
+        } else if(ch.isDigit() || ch in listOf('+', '-', '.')){
+            //A floating-point number
+            return this._parse_float_token(first_char=ch.toString(), token_location=token_location)
+        } else if(ch.isLetter() || ch == '_'){
+            //Since it begins with an alphabetic character, it must either be a keyword or an identifier
+            return this._parse_keyword_or_identifier_token(first_char=ch.toString(), token_location=token_location)
+        } else{
+            //We got some weird character, like '@` or `&`
+            throw GrammarError(this.location, message = "Invalid character $ch")
+        }
+    }
+
+    /**
+     *        """Make as if `token` were never read from `input_file`"""
+     */
+    fun unread_token(token:Token){
+        require(this.saved_token != null)
+        this.saved_token = token
+    }
+
+    private fun _parse_string_token(token_location: SourceLocation): Token.StringToken {
+
+        var token:String = ""
+        while(true) {
+            val ch = this.read_char()
+
+            if (ch == '"') break
+
+            if (ch == '\u0000') throw GrammarError(token_location, "unterminated string")
+
+            token += ch.toString()
+        }
+
+        return Token.StringToken(token_location, token)
+    }
+
+    private fun _parse_float_token(first_char: String, token_location: SourceLocation): Token.LiteralNumberToken {
+
+        var token: String = first_char
+        while (true) {
+            val ch = this.read_char()
+
+            if (!(ch.isDigit() || ch == '.' || ch in listOf('e', 'E'))) {
+                this.unread_char(ch)
+                break
+            }
+
+            token += ch
+        }
+
+        val value:Float
+
+        try {
+            value = token.toFloat()
+        } catch (e: NumberFormatException) {
+            throw GrammarError(token_location, "'$token' is an invalid floating-point number")
+        }
+
+        return Token.LiteralNumberToken(token_location, value)
+
+    }
+
+    private fun _parse_keyword_or_identifier_token(first_char: String, token_location: SourceLocation): Token{
+
+            var token = first_char
+            while (true) {
+                val ch = this.read_char()
+                //Note that here we do not call "isLetter" but "isLetterOrDigit": digits are ok after the first character
+                if (!(ch.isLetterOrDigit() || ch == '_')) {
+                    this.unread_char(ch)
+                    break
+                }
+                token += ch
+            }
+
+            try {
+                // If it is a keyword, it must be listed in the KEYWORDS map
+                return Token.KeywordToken(token_location, KEYWORDS[token] ?: throw NoSuchElementException())
+            } catch (e: NoSuchElementException) {
+                // If we got NoSuchElementException, it is not a keyword, and thus it must be an identifier
+                return Token.IdentifierToken(token_location, token)
+            }
     }
 
 }
--- src/test/kotlin/InputStreamTest.kt
+++ src/test/kotlin/InputStreamTest.kt
@@ -1,10 +1,14 @@
+import org.example.GrammarError
+import org.example.KEYWORDS
+import org.example.KeywordEnum
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
+import org.example.Token
 import java.io.ByteArrayInputStream
 import org.example.InputStream as InputStream
 import java.io.InputStream as InputStream1
 
-class InputStreamTest{
+class InputStreamTest {
 
     @Test
     fun testInputFile() {
@@ -53,5 +57,65 @@ class InputStreamTest{
         assertTrue(stream.location.col_num == 3)
 
         assertTrue(stream.read_char() == '\u0000')
+    }
+
+    @Test
+    fun test_lexer() {
+        val myString: String = """
+       # This is a comment
+       # This is another comment
+        new material sky_material(
+            diffuse(image("my file.pfm")),
+            <5.0, 500.0, 300.0>
+        ) # Comment at the end of the line
+    """
+        val stream: InputStream = InputStream(myString.byteInputStream())
+
+        stream.read_token()?.let { _assert_is_keyword(it, KeywordEnum.NEW) }
+        stream.read_token()?.let { _assert_is_keyword(it, KeywordEnum.MATERIAL) }
+        stream.read_token()?.let { _assert_is_identifier(it, "sky_material") }
+        stream.read_token()?.let { _assert_is_symbol(it, "(") }
+        stream.read_token()?.let { _assert_is_keyword(it, KeywordEnum.DIFFUSE) }
+        stream.read_token()?.let { _assert_is_symbol(it, "(") }
+        stream.read_token()?.let { _assert_is_keyword(it, KeywordEnum.IMAGE) }
+        stream.read_token()?.let { _assert_is_symbol(it, "(") }
+        stream.read_token()?.let { _assert_is_string(it, "my file.pfm") }
+        stream.read_token()?.let { _assert_is_symbol(it, ")") }
+    }
+
+    private fun _assert_is_string(token: Token, string: String) {
+        if(token !is Token.StringToken) {
+            throw AssertionError("Token '$token' is not a StringToken")
+        }
+        if(token.string != string){
+            throw AssertionError("expecting symbol '$string' instead of '$token'")
+        }
+    }
+
+    private fun _assert_is_symbol(token: Token, symbol: String) {
+        if(token !is Token.SymbolToken) {
+            throw AssertionError("Token '$token' is not a SymbolToken")
+        }
+        if(token.symbol != symbol){        
+            throw AssertionError("expecting symbol '$symbol' instead of '$token'")
+        }
+    }
+
+    private fun _assert_is_identifier(token: Token, identifier: String) {
+        if(token !is Token.IdentifierToken){
+            throw AssertionError("Token '$token' is not an IdentifierToken")
+        }
+        if(token.identifier != identifier) {
+            throw AssertionError("expecting identifier '$identifier' instead of '$token'")
+        }
+    }
+
+    private fun _assert_is_keyword(token: Token, keyword: KeywordEnum) {
+        if (token !is Token.KeywordToken) {
+            throw AssertionError("Token '$token' is not a KeywordToken")
+        }
+        if (token.keyword != keyword) {
+            throw AssertionError("Token '${token.keyword}' is not equal to keyword '$keyword'")
+        }
     }
 }
